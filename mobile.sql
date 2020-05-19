495a9cdb43c4376ae07a33c8add59e629b5661b1
Mohtar <mohtarnurwahid@gmail.com>
2020-05-19 13:30:55 +0700

    nambah sql
    nambah sql untuk percoban

---
+++ mobile.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Thursday-January-30-2020   
+--------------------------------------------------------
+REM INSERTING into IBIS.ACL_MENU_MOBILE
+SET DEFINE OFF;
+Insert into IBIS.ACL_MENU_MOBILE (ID_MENU,MENU_NAME,SERVICE_NAME,ICON,PRIORITY,LINK_MENU,TERMINAL) values (31,'Vessel Schedule','Non-Container','./img/vs.png',3,'to_vessel_schedule_nc','IDBTN');
+Insert into IBIS.ACL_MENU_MOBILE (ID_MENU,MENU_NAME,SERVICE_NAME,ICON,PRIORITY,LINK_MENU,TERMINAL) values (32,'Track & Trace','Non-Container','./img/ct.png',1,'to_non_container_tracking','IDBTN');
+Insert into IBIS.ACL_MENU_MOBILE (ID_MENU,MENU_NAME,SERVICE_NAME,ICON,PRIORITY,LINK_MENU,TERMINAL) values (33,'TCA','Non-Container','./img/tca.png',2,'to_truck_container_association_nc','IDBTN');
+Insert into IBIS.ACL_MENU_MOBILE (ID_MENU,MENU_NAME,SERVICE_NAME,ICON,PRIORITY,LINK_MENU,TERMINAL) values (34,'Tarif Information','Non-Container','./img/ts.png',4,'to_tariff_information','IDBTN');
+Insert into IBIS.ACL_MENU_MOBILE (ID_MENU,MENU_NAME,SERVICE_NAME,ICON,PRIORITY,LINK_MENU,TERMINAL) values (35,'Notification','Non-Container','./img/ring.png',4,'to_notification','IDBTN');
